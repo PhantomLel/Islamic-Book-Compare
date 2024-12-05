7ae9dbd1ab6ec00e16095130fb85f79cc28e6f92
Abdullah Mohammad <abdullahmohammad@Asims-MacBook-Pro-2.local>
2024-12-05 11:23:11 -0600

    added more data to ip stuff

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -12,14 +12,16 @@ export const handle: Handle = async ({ event, resolve }) => {
     const db = await getDb();
     const date = new Date().toLocaleDateString("en-US", { year: '2-digit', month: '2-digit', day: '2-digit' });
     const ip = event.request.headers.get('x-forwarded-for')?.split(',')[0].trim() || 'Unknown IP';
+    const userAgent = event.request.headers.get('user-agent') || 'Unknown User Agent';
+    const url = event.request.url;
 
     const ip_info = await db.collection('ips').findOne({ date });
     if (!ip_info) {
-        await db.collection('ips').insertOne({ date, count: 1, ips: [ip] });
+        await db.collection('ips').insertOne({ date, count: 1, data: [{ ip, userAgent, url }] });
     } else {
         await db.collection('ips').updateOne(
             { date }, 
-            { $inc: { count: 1 }, $push: { ips: ip } } as any
+            { $inc: { count: 1 }, $push: { data: { ip, userAgent, url } } } as any
         );
     }
 
